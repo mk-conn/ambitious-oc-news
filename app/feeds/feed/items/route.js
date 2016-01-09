88addb0b4cac1e3ec65ae08d0777bd6cfdc7911d
Marko Krüger <mk@marko-krueger.de>
2016-01-09 09:41:38 +0100

    * reset _offset

--- app/feeds/feed/items/route.js
+++ app/feeds/feed/items/route.js
@@ -28,12 +28,16 @@ export default Route.extend(InfinityRoute, {
     set(model, 'feed', this.modelFor('feeds.feed'));
   },
   afterInfinityModel(items) {
+    //console.log('route.js:afterInfinityModel', get(this, '_offset'));
     const lastObjectId = items.get('lastObject.id');
     const loadedAny = items.get('length') > 0;
     this.set('_canLoadMore', loadedAny);
-
     this.set('_offset', lastObjectId);
+  },
+  actions: {
+    willTransition() {
+      this.set('_offset', undefined);
+    }
   }
-
 
 });
