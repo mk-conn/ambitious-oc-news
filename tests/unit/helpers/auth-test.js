96028e9a47dae6c326a7c962940d4be206865cd2
Marko Krüger <mk@marko-krueger.de>
2016-01-15 19:40:44 +0100

    cleanup

---
+++ tests/unit/helpers/auth-test.js
@@ -0,0 +1,10 @@
+import { auth } from '../../../helpers/auth';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | auth');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = auth([42]);
+  assert.ok(result);
+});
