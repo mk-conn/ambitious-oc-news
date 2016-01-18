1699a93ed68338f0229cdabe59860aed2d1b77d4
Krüger, Marko <mk@marko-krueger.de>
2016-01-18 11:38:32 +0100

    * bookmarks (header, counter label)

--- app/components/starred-items/component.js
+++ app/components/starred-items/component.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 import Counting from 'ambitious-oc-news/mixins/unread-read';
 
+const {inject, get, computed} = Ember;
+
 export default Ember.Component.extend(Counting, {
+  meta: inject.service(),
+  starredCount: computed.alias('meta.starredCount'),
   classNames: ['list-group-item', 'feed-item', 'feed-item-link'],
   click() {
     this.get('app').transitionTo('feeds.items.starred');
