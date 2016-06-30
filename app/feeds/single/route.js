95dd38890a11ae192af978f27f26233422b3e0e7
Marko Krüger <mk@marko-krueger.de>
2016-07-01 00:38:55 +0200

    layouting with flexi

--- app/feeds/single/route.js
+++ app/feeds/single/route.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
+import Protected from 'ambitious-oc-news/mixins/protected';
+import ResetToTop from 'ambitious-oc-news/mixins/reset-to-top';
 
-export default Ember.Route.extend({
+const {Route} = Ember;
+
+export default Route.extend(Protected, ResetToTop, {
+  model(params) {
+    return this.store.peekRecord('feed', params.feed_id);
+  },
+  // renderTemplate() {
+  //   this.render('feeds/feed', {
+  //     into: 'application',
+  //     outlet: 'main'
+  //   });
+  // },
+  actions: {
+    delete() {
+      let feed = this.modelFor(this.routeName);
+
+      feed.deleteRecord();
+
+      if (feed.get('isDeleted')) {
+        feed.save().then(() => {
+          this.transitionTo('feeds');
+        });
+      }
+    }
+  }
 });
