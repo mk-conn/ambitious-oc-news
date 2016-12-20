ef51ea3fbd2f98f31342642408fee9d5fd3a6add
Marko Krüger <mk@marko-krueger.de>
2016-12-20 22:56:56 +0100

    pinned feed articles

--- app/components/pinned-articles/component.js
+++ app/components/pinned-articles/component.js
@@ -5,9 +5,6 @@ const {inject, get, computed} = Ember;
 
 export default Ember.Component.extend(Counting, {
   meta: inject.service(),
+
   starredCount: computed.alias('meta.starredCount'),
-  classNames: ['list-group-item', 'feed-item', 'feed-item-link'],
-  click() {
-    this.get('app').transitionTo('feeds.items.starred');
-  }
 });
